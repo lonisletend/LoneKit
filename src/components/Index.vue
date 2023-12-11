ce419f370cfec54b577fd11b712c0140f85bf4ac
lonisletend <yangxjsun0324@gmail.com>
2023-12-12 02:34:37 +0800

    :sparkles: TimeTool & JsonTool

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -1,34 +1,38 @@
 <template>
   <div class="w-full h-full flex">
-    <n-layout-sider
-      bordered
-      collapse-mode="width"
-      :collapsed-width="64"
-      :width="200"
-      :collapsed="collapsed"
-      show-trigger
-      @collapse="collapsed = true"
-      @expand="collapsed = false"
-    >
-      <div class="w-full h-16 mt-2 flex items-center justify-center font-bold text-2xl text-green-600 cursor-pointer select-none"
-           v-if="!collapsed">
-        <span>LoneKit</span>
-      </div>
-      <div class="w-full h-16 mt-2 flex items-center justify-center font-bold text-2xl text-green-600 cursor-pointer select-none"
-           v-else>
-        <span>LK</span>
-      </div>
-      <n-menu
-        v-model:value="activeKey"
-        :collapsed="collapsed"
+    <n-layout has-sider>
+      <n-layout-sider
+        bordered
+        collapse-mode="width"
         :collapsed-width="64"
-        :collapsed-icon-size="22"
-        :options="menuOptions"
-      />
-    </n-layout-sider>
-    <div class="w-full h-full px-8 py-4 overflow-scroll">
-      <router-view />
-    </div>
+        :width="200"
+        :collapsed="collapsed"
+        show-trigger
+        @collapse="collapsed = true"
+        @expand="collapsed = false"
+      >
+        <div class="w-full h-16 mt-2 flex items-center justify-center font-bold text-2xl text-green-600 cursor-pointer select-none"
+             v-if="!collapsed">
+          <span>LoneKit</span>
+        </div>
+        <div class="w-full h-16 mt-2 flex items-center justify-center font-bold text-2xl text-green-600 cursor-pointer select-none"
+             v-else>
+          <span>LK</span>
+        </div>
+        <n-menu
+          v-model:value="activeKey"
+          :collapsed="collapsed"
+          :collapsed-width="64"
+          :collapsed-icon-size="22"
+          :options="menuOptions"
+        />
+      </n-layout-sider>
+      <div class="w-full h-full px-4 py-2 overflow-scroll">
+        <n-notification-provider>
+          <router-view />
+        </n-notification-provider>
+      </div>
+    </n-layout>
   </div>
 </template>
 
@@ -36,7 +40,7 @@
 import router from "../router";
 import { h, ref } from "vue";
 import { RouterLink } from "vue-router";
-import { NLayout, NLayoutSider, NMenu, NSpace, NSwitch, NIcon } from "naive-ui";
+import {NLayout, NLayoutSider, NMenu, NIcon, NNotificationProvider} from "naive-ui";
 import { TimeOutline as TimeIcon } from "@vicons/ionicons5";
 import { Braces24Filled as JsonIcon } from "@vicons/fluent";
 
--- src/components/tools/TimeTool.vue
+++ src/components/tools/TimeTool.vue
@@ -1,40 +1,200 @@
 <template>
-  <div class="w-full h-full">
-    Lorem ipsum dolor sit amet, saperet nostrum molestiae ei usu, accumsan laboramus duo ex. Causae mandamus dissentiunt eu mei, te ullum ceteros assueverit sit. Ad vivendo gubergren adversarium vel. Debitis tibique ad has, pro et dicta prompta detraxit. Omnes feugait atomorum sea ei, est quod delectus inimicus ne.
-
-    Vidit eruditi duo in, eu nominavi tractatos periculis has. Ad saepe soleat eos. Ex mei suas assueverit dissentiet, pri ornatus fuisset sensibus ad. Choro eripuit sed ut, ei causae singulis electram sed, ea erant evertitur sed. Quo cu ullum singulis.
-
-    Urbanitas mnesarchum an ius, no nihil maiestatis moderatius pro. Ullum utroque te eam, ius no quaeque platonem, in mea exerci maiestatis. Solum pertinax contentiones ne est, ea justo impedit detraxit vel. Eam harum sonet appellantur cu. Pri an solet utinam salutandi, aeque expetenda corrumpit ei est.
-
-    Sumo viris his cu, vix cu solet putent possim, cu modus volutpat consequuntur pro. Meis voluptua cotidieque ex qui, mel ea quodsi tibique. Ea mea primis conclusionemque, eos te nobis quodsi. Eu nam quod aperiam sadipscing, sit falli dicant ea, duo impedit incorrupte necessitatibus id. Eu regione mediocritatem sed.
-
-    Sed electram percipitur efficiantur ei. Ex eros falli percipitur pri, cu per eripuit explicari. Mei commodo evertitur et. Est no diam saperet graecis. Soluta tempor voluptua in per, vel eius dissentias ut.
-
-    Pri accusata repudiare in, nullam splendide te sea. Quo mucius iudicabit delicatissimi et, solum munere bonorum ad per. Mel ad quis aliquip discere. Viris latine aliquid mea at, eos eu utinam theophrastus necessitatibus, summo aliquip nam an. Nam ei sapientem erroribus molestiae, at hinc appetere pertinax eam, quot mutat contentiones ea vel. Vel duis soluta gubergren eu.
-
-    Et pri maiorum antiopam consetetur, in cum dolor deseruisse voluptatibus, sit et nobis petentium delicatissimi. Sale vidit partem ut cum, nam sale iudicabit delicatissimi id, ut eam dicam eripuit principes. Eirmod sensibus eos ea, dicat utamur et per. Eos te mucius sanctus, in aliquid comprehensam nam, eu mel veri altera audire. Pro no accusam recteque imperdiet.
-
-    Timeam fabulas delectus sea an, modus intellegebat sed ei. Stet atqui principes no usu, pri in dissentiunt mediocritatem, eum illum detracto consequat eu. Cu vocibus deleniti usu, ex sint consul nullam quo, modo salutandi qualisque pri id. Diam graeco deterruisset ex nam, cu mei latine concludaturque. Ubique appareat ut mel, ne quot fierent mel. Eu mei hinc minimum vituperatoribus, modo soleat vis ex.
-
-    At quo eripuit dignissim. Est accusata ocurreret te, an quis aliquam vel. Ne has erat deserunt. Sed magna volumus deserunt eu, in diam illud per. Ut primis luptatum efficiantur nam, veri disputando sea ea, audiam voluptua delicatissimi qui ut.
-
-    Diceret civibus convenire et cum, est id diam debitis suscipiantur. Sit an utamur lucilius, ridens legendos indoctum ut duo. Nostro consulatu id usu. Eu eam dicta efficiendi, eam liber sapientem id. Usu ad suas fastidii volutpat.
-
+  <div class="w-full h-full space-y-4">
+    <n-input-group>
+      <n-input-group-label :style="{ width: '100px', 'text-align': 'center' }">输入时间</n-input-group-label>
+      <n-input :style="{ width: '50%' }" placeholder="输入格式任意时间" :status="status.inputTime" clearable
+               v-model:value="inputTime" @input="handleInputTimeChange" />
+      <n-button type="primary" ghost :style="{ width: '80px', 'text-align': 'center' }" @click="timeNow">当前时间</n-button>
+    </n-input-group>
+    <n-input-group>
+      <n-input-group-label :style="{ width: '100px', 'text-align': 'center' }">时间戳(s)</n-input-group-label>
+      <n-input-number :show-button="false" :style="{ width: '50%' }" placeholder="" :status="status.timestamp" clearable
+                      v-model:value="timestamp" @update:value="handleTimestampChange" />
+      <n-button type="primary" ghost :style="{ width: '80px', 'text-align': 'center' }" @click="copyTimestamp">复制</n-button>
+    </n-input-group>
+    <n-input-group>
+      <n-input-group-label :style="{ width: '100px', 'text-align': 'center' }">时间戳(ms)</n-input-group-label>
+      <n-input-number :show-button="false" :style="{ width: '50%' }" placeholder="" :status="status.milliTimestamp" clearable
+                      v-model:value="milliTimestamp" @update:value="handleMilliTimestampChange" />
+      <n-button type="primary" ghost :style="{ width: '80px', 'text-align': 'center' }" @click="copyMilliTimestamp">复制</n-button>
+    </n-input-group>
+    <n-input-group>
+      <n-input-group-label :style="{ width: '100px', 'text-align': 'center' }">日期</n-input-group-label>
+      <n-date-picker v-model:value="milliTimestamp" type="date" :style="{ width: '50%' }" placeholder="选择日期" clearable
+                     @update:value="handleMilliTimestampChange"/>
+      <n-button type="primary" ghost :style="{ width: '80px', 'text-align': 'center' }" @click="copyDate">复制</n-button>
+    </n-input-group>
+    <n-input-group>
+      <n-input-group-label :style="{ width: '100px', 'text-align': 'center' }">日期时间</n-input-group-label>
+      <n-date-picker v-model:value="milliTimestamp" type="datetime" :style="{ width: '50%' }" placeholder="选择日期时间" clearable
+                     @update:value="handleMilliTimestampChange"/>
+      <n-button type="primary" ghost :style="{ width: '80px', 'text-align': 'center' }" @click="copyDateTime">复制</n-button>
+    </n-input-group>
   </div>
 </template>
 
 <script setup>
-import { ref, onMounted } from "vue";
+import { ref, onMounted, reactive } from "vue";
+import { NInputGroup, NInputGroupLabel, NInput, NInputNumber, NButton, NDatePicker, NNotificationProvider } from "naive-ui";
+import { useNotification } from "naive-ui";
+import dayjs from "dayjs";
+import { writeText } from "@tauri-apps/api/clipboard";
 
 const inputTime = ref();
 const timestamp = ref();
 const milliTimestamp = ref();
-const dateStr = ref();
-const datetimeStr = ref();
+
+const status = reactive({
+  inputTime: '',
+  timestamp: '',
+  milliTimestamp: '',
+});
 
 onMounted(() => {
 })
 
+function handleInputTimeChange(val) {
+  console.log('Time change val ==> ', val);
+  console.log('Time change value ==> ', inputTime.value);
+  if (inputTime.value === '') {
+    status.inputTime = '';
+    initResult();
+    return;
+  }
+  if (isTimestamp(inputTime.value)) {
+    status.inputTime = '';
+    transfer(dayjs.unix(Number.parseInt(inputTime.value)));
+    return;
+  }
+  if (isMilliTimestamp(inputTime.value)) {
+    status.inputTime = '';
+    transfer(dayjs(Number.parseInt(inputTime.value)));
+    return;
+  }
+  if (isDate(inputTime.value)) {
+    status.inputTime = '';
+    transfer(dayjs(inputTime.value + ' 00:00:00', 'YYYY-MM-DD HH:mm:ss'));
+    return;
+  }
+  if (isDatetime(inputTime.value)) {
+    status.inputTime = '';
+    transfer(dayjs(inputTime.value, 'YYYY-MM-DD HH:mm:ss'));
+    return;
+  }
+  status.inputTime = 'error';
+  initResult();
+  console.log('Time change status ==> ', status);
+}
+
+function handleTimestampChange(val) {
+  console.log('Timestamp change val ==> ', val);
+  if (val && !isTimestamp(val.toString())) {
+    status.timestamp = 'error';
+    inputTime.value = '';
+    milliTimestamp.value = null;
+  } else {
+    status.timestamp = '';
+    inputTime.value = val.toString();
+    handleInputTimeChange(val.toString());
+  }
+}
+
+function handleMilliTimestampChange(val) {
+  console.log('MilliTimestamp change val ==> ', val);
+  if (val && !isMilliTimestamp(val.toString())) {
+    status.milliTimestamp = 'error';
+    inputTime.value = '';
+    timestamp.value = null;
+  } else {
+    status.milliTimestamp = '';
+    const value = val ? val.toString() : '';
+    inputTime.value = value;
+    handleInputTimeChange(value);
+  }
+}
+
+function initResult() {
+  timestamp.value = null;
+  milliTimestamp.value = null;
+}
+
+function transfer(day) {
+  console.log('Transfer ==> ', day);
+  if (day.isValid()) {
+    timestamp.value = day.unix();
+    milliTimestamp.value = day.valueOf();
+  } else {
+    timestamp.value = 'ERROR';
+    milliTimestamp.value = 'ERROR';
+  }
+}
+
+function timeNow() {
+  let now = dayjs();
+  inputTime.value = now.format('YYYY-MM-DD HH:mm:ss');
+  transfer(now);
+}
+
+function isNumeric(value) {
+  return /^-?\d+$/.test(value);
+}
+
+function isTimestamp(str) {
+  if (isNumeric(str) && str.length === 10) {
+    return Number.parseInt(str).toString().length === 10;
+  }
+  return false;
+}
+
+function isMilliTimestamp(str) {
+  if (isNumeric(str) && str.length === 13) {
+    return Number.parseInt(str).toString().length === 13;
+  }
+  return false;
+}
+
+function isDate(str) {
+  let dateReg = /^[1-9]\d{3}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])$/;
+  return dateReg.test(str);
+}
+
+function isDatetime(str) {
+  let datetimeReg = /^[1-9]\d{3}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])\s+(2[0-3]|[0-1]\d):[0-5]\d:[0-5]\d$/;
+  return datetimeReg.test(str);
+}
+
+function copyTimestamp() {
+  copy(timestamp.value);
+}
+function copyMilliTimestamp() {
+  copy(milliTimestamp.value);
+}
+function copyDate() {
+  copy(dayjs(milliTimestamp.value).format('YYYY-MM-DD'));
+}
+function copyDateTime() {
+  copy(dayjs(milliTimestamp.value).format('YYYY-MM-DD HH:mm:ss'));
+}
+
+const notification = useNotification();
+function notify(type, message) {
+  notification[type]({
+    content: message,
+    // meta: "想不出来",
+    duration: 2500,
+    keepAliveOnHover: true
+  });
+}
+
+function copy(value) {
+  if (navigator && navigator.clipboard) {
+    navigator.clipboard.writeText(value);
+  }
+  if (window.__TAURI_IPC__) {
+    writeText(value.toString());
+  }
+  notify('success', '复制成功!');
+}
 </script>
 
 <style>
