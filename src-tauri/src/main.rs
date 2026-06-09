5392b4b214c3d4b3536670430df00e00fbef4460
lonisletend <yangxjsun0324@gmail.com>
2026-06-09 10:06:30 +0800

    fix: enable updater relaunch support

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -10,6 +10,7 @@ fn greet(name: &str) -> String {
 fn main() {
     tauri::Builder::default()
         .plugin(tauri_plugin_shell::init())
+        .plugin(tauri_plugin_process::init())
         .plugin(tauri_plugin_global_shortcut::Builder::new().build())
         .plugin(tauri_plugin_updater::Builder::new().build())
         .plugin(tauri_plugin_clipboard_manager::init())
@@ -21,4 +22,3 @@ fn main() {
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
-
